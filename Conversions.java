47ff0eb73227aed3e7b5be83e8d7a832d8990944
jmkoelemay <jmkoelemay01@bvsd.org>
2025-08-20 18:56:40 +0000

    Added Menu

--- Conversions.java
+++ Conversions.java
@@ -1,1 +1,16 @@
+// Jonathan Koelemay
+// 08/20/25
 
+public class Conversions 
+{
+
+    public static void main(String[] args) 
+    {
+        System.out.println("1. Fahrenheit to Celsuis");
+        System.out.println("2. Celsuis to Fahrenheit");
+        System.out.println("3. Feet to Meters");
+        System.out.println("4. Meters to Feet");
+        System.out.println("5. Ounces to Milliliters");
+        System.out.println("6. Milliliters to Ounces");
+    }
+}
